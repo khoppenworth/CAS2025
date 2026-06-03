86c9cc9811f1fd2223092e9b0d4c73fb5e28b98c
Kim Hoppenworth <khoppenworth@msh.org>
2026-06-03 10:09:06 -0400

    Fix next assessment date action label

--- admin/pending_accounts.php
+++ admin/pending_accounts.php
@@ -177,7 +177,7 @@ foreach ($activeStaff as $staffRow) {
           <th><?=t($t,'email','Email')?></th>
           <th><?=t($t,'next_assessment','Next Assessment')?></th>
           <th><?=t($t,'last_approved','Approved On')?></th>
-          <th><?=t($t,'change_next_aseessment_date','Change Next Aseessment Date')?></th>
+          <th><?=t($t,'change_next_assessment_date','Change Next Assessment Date')?></th>
         </tr>
       </thead>
       <tbody>
